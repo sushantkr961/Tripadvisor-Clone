74048e164e16da5e3d6f1ede7ff09bfbc4e77711
Nipendra Soni <rsoni2843@gmail.com>
2022-11-09 13:43:07 +0530

    api

--- src/Components/Nipendra/Login.jsx
+++ src/Components/Nipendra/Login.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+const hotelSearchAPI =
+  "https://voyagerx.goibibo.com/api/v1/hotels_search/find_node_by_name/?search_query=Delhi&limit=10&qt=N&inc_ggl_res=true&add_t=region";
+
+const hotelPageAPI =
+  "https://hermes.goibibo.com/hotels/v13/search/data/v3/9114123290289445387/20221112/20221113/1-2-0?s=popularity&cur=INR&tmz=-330";
+
 const Login = () => {
   return <div>Login</div>;
 };
