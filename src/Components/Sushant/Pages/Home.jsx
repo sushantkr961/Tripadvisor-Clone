a7e7586a11dd7b42161bd56480609a0a65ad2d89
sushantkr961 <sushantonly961@gmail.com>
2022-11-10 10:53:53 +0530

    ok

--- src/Components/Sushant/Pages/Home.jsx
+++ src/Components/Sushant/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import IndiaTour from '../Components/IndiaTour';
 import Navbar from '../Components/Navbar';
+import Searchbar from '../Components/Searchbar';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Searchbar />
+      <IndiaTour />
     </div>
   )
 }
