cc22b6613eccbdafa22a94601ff2dc0408a656a7
Nipendra Soni <rsoni2843@gmail.com>
2022-11-11 22:43:03 +0530

    day-4 work login and signup

--- Frontend/src/Components/Nipendra/Users/MyComponents/SocialLogin.jsx
+++ Frontend/src/Components/Nipendra/Users/MyComponents/SocialLogin.jsx
@@ -2,11 +2,25 @@ import { Box, Button, Stack } from "@chakra-ui/react";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
+import {
+  signInWithFacebook,
+  signInWithGoogle,
+} from "./../../../../Redux/Users/Login/login.action";
+import { useDispatch } from "react-redux";
 const SocialLogin = () => {
+  const dispatch = useDispatch();
+  const handleGoogle = () => {
+    dispatch(signInWithGoogle());
+  };
+  const handleFacebook = () => {
+    dispatch(signInWithFacebook());
+  };
+
   return (
     <Box w={"95%"} m={"auto"} mt={4}>
       <Stack variant="outline" w={"full"} direction="column" spacing={4}>
         <Button
+          onClick={handleGoogle}
           bg="white"
           border={"0.5px solid black"}
           rounded={"full"}
@@ -16,6 +30,7 @@ const SocialLogin = () => {
         </Button>
 
         <Button
+          onClick={handleFacebook}
           bg="white"
           border={"0.5px solid black"}
           rounded={"full"}
