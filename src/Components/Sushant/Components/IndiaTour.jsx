e272399042ac41d07db11c3f7da6cae739d05ec0
sushantkr961 <sushantonly961@gmail.com>
2022-11-12 10:20:33 +0530

    ok

--- src/Components/Sushant/Components/IndiaTour.jsx
+++ src/Components/Sushant/Components/IndiaTour.jsx
@@ -40,7 +40,10 @@ const IndiaTour = () => {
           <Img src='https://th.bing.com/th/id/OIP.sP9RtX_qphHskOup8F_gIAHaEK?w=324&h=182&c=7&r=0&o=5&dpr=1.1&pid=1.7' w='400px' h='200px' />
         </Box>
       </Box>
-      <Box display='flex' mt='50px'>
+      <Box w='89%' margin='auto' mt='50px'>
+        <Text textAlign='left' fontSize='24px' fontWeight='bold'>Monuments in India</Text>
+      </Box>
+      <Box display='flex' >
         <Carousel breakPoints={breakPoints}>
           <Card src='https://www.bing.com/th/id/OGC.de5cf6f9ea0a743acdd5dbabdd88a68b?pid=1.7&rurl=https%3a%2f%2fohfact.com%2fwp-content%2fuploads%2f2015%2f08%2fTaj-Mahal.gif&ehk=tA%2bIlUQggKPEmpaeu0ULuA7ssgsr28CpwzFiy4%2fPpZU%3d' name='The Taj Mahal' />
           <Card src='https://th.bing.com/th/id/OIP.tTH3jF3k-JWfZdbe6cakdgHaFj?w=264&h=198&c=7&r=0&o=5&dpr=1.1&pid=1.7' name='India Gate' />
