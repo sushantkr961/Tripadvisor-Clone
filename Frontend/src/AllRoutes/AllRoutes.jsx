e2715262372b9edfce9ef7b4818bbfa1c4d9c888
Nipendra Soni <rsoni2843@gmail.com>
2022-12-21 22:25:25 +0530

    day1 folder structure updated

--- Frontend/src/AllRoutes/AllRoutes.jsx
+++ Frontend/src/AllRoutes/AllRoutes.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { HotelListG } from "../Components/Gaurav/HotelListG";
-import { SingleRestaurant } from "../Components/Gaurav/SingleRestaurant";
-import CheckPage from "../Components/Nipendra/Users/CheckPage";
 import Login from "../Components/Nipendra/Users/Login";
-import PaymentDetails from "../Components/Ravi/PaymentDetails";
-import TravellerDetails from "../Components/Ravi/TravellerDetails";
 import Home from "../Components/Sushant/Pages/Home";
 
 const AllRoutes = () => {
@@ -14,11 +9,6 @@ const AllRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/check" element={<CheckPage />} />
-        <Route path="/hotelPage" element={<SingleRestaurant />} />
-        <Route path="/hotelLists" element={<HotelListG />} />
-        <Route path="/payment" element={<PaymentDetails />} />
-        <Route path="/traveller" element={<TravellerDetails />} />
       </Routes>
     </div>
   );
--- Frontend/src/Components/Gaurav/ReactIcon.jsx
+++
@@ -1,6 +0,0 @@
-import { Image } from "@chakra-ui/react"
-
-
-export const  SearchIcon =()=>{
-    return <Image src=""/>
-}
