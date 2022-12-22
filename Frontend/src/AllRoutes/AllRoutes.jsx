f13b09866006b55ef83d0d3695231efceee1b8e2
sushantkr961 <sushantonly961@gmail.com>
2022-12-22 23:50:27 +0530

    conflicts resolved

--- Frontend/src/AllRoutes/AllRoutes.jsx
+++ Frontend/src/AllRoutes/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { HotelList } from "../Components/HotelList/Hotellist";
 import Login from "../Components/Nipendra/Users/Login";
 import Home from "../Components/Sushant/Pages/Home";
 
@@ -9,6 +10,7 @@ const AllRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path={"/hotellist"} element={<HotelList/>}/>
       </Routes>
     </div>
   );
