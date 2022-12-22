cfc12e66a1d4775670c63c4e3038449a39548f80
Nipendra Soni <rsoni2843@gmail.com>
2022-12-22 20:21:41 +0530

    showing conflicts

--- Backend/src/middlewares/AdminMiddleware/admin.middleware.ts
+++ Backend/src/middlewares/AdminMiddleware/admin.middleware.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response } from "express";
-
 import UserModel from "../../modules/User/user.model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 class AdminController {
-  static adminLogin = async (req: Request, res: Response) => {
+  static adminLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
       const { email, password } = req.body;
       // Checking both if email & password are present
@@ -52,7 +55,6 @@ class AdminController {
       res.status(408).send({ Message: "Unable to Login" });
     }
   };
-
   static getAllUsers = async (req: Request, res: Response) => {
     const allUsers = await UserModel.find();
     try {
