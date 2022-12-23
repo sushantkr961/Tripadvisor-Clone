4b54f50e97d49c44b2d6810d2441f13e12721dea
sushantkr961 <sushantonly961@gmail.com>
2022-12-23 21:07:23 +0530

    error page done tourism page done

--- Frontend/src/AllRoutes/AllRoutes.jsx
+++ Frontend/src/AllRoutes/AllRoutes.jsx
@@ -3,8 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import { HotelList } from "../Components/HotelList/Hotellist";
 import Login from "../Components/Nipendra/Users/Login";
 import Home from "../Components/Sushant/Pages/Home";
-import HotelDetails from "../Components/Sushant/Pages/HotelDetails";
 import Profile from "../Components/Nipendra/Profile/Profile";
+import Bihar from "../Components/Sushant/Pages/Bihar";
+import ErrorPage from "../Components/Sushant/Pages/ErrorPage";
 
 const AllRoutes = () => {
   return (
@@ -13,8 +14,9 @@ const AllRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path={"/hotellist"} element={<HotelList />} />
-        <Route path={"/hoteldetails"} element={<HotelDetails />} />
         <Route path={"/profile/*"} element={<Profile />} />
+        <Route path={"/bihartourism"} element={<Bihar />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
---
+++ Frontend/src/Components/Sushant/Pages/Bihar.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import bihar from "../assests/bihar.mp4";
+import {
+  AspectRatio,
+  Box,
+  Button,
+  chakra,
+  Flex,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+
+const Bihar = () => {
+  return (
+    <Box w={{ base: "100%", md: "70%" }} margin="auto">
+      <AspectRatio ratio={16 / 9} mb="30px">
+        <video src={bihar} autoPlay loop muted />
+      </AspectRatio>
+      <Box>
+        <Box>
+          <chakra.h3
+            fontFamily={"Work Sans"}
+            fontWeight={"bold"}
+            fontSize={30}
+            textAlign={"left"}
+            color={"black"}
+          >
+            Unlock Bihar
+          </chakra.h3>
+          <Text>Extraordinary places to visit</Text>
+
+          <Flex flexDir={{ base: "column", lg: "row" }} gap={5} border={"1px"}>
+            <Box border={"1px"} w={{ base: "100%", md: "80%", lg: "90%" }}>
+              <chakra.h5
+                fontFamily={"Work Sans"}
+                fontWeight={"bold"}
+                fontSize={20}
+                textAlign={"left"}
+                color={"black"}
+                mt={"10px"}
+                mb={"10px"}
+              >
+                Balmiki Tiger Reserve
+              </chakra.h5>
+              <Text mb={"50px"}>
+                Balmiki Tiger Reserve forms the eastern most limit of the
+                Himalayan Terai forests in India, and is the only tiger reserve
+                of Bihar. Situated in the Gangetic Plains bio-geographic zone of
+                the country, the forest has combination of bhabar and terai
+                tracts. Valmiki Tiger Reserve lies in the north-westernmost West
+                Champaran district of Bihar. Name of the district has been
+                derived from two words Champa and Aranya meaning Forest of
+                Champa trees. Wild mammals found in the forests of Valmiki Tiger
+                Reserve are Tiger, Sloth bear, Leopard, Wild dog, Bison, Wild
+                boar etc. Several species of deer and antelopes viz barking
+                deer, spotted deer, hog deer, sambar and blue bull are also
+                found here. In Madanpur forest block large number of Indian
+                flying foxes can be sighted. The Reserve has rich avi-fauna
+                diversity. Over 250 species of birds have been reported. Valmiki
+                Sanctuary covers about 800 square kilometres (310 sq mi) of
+                forest and was the 18th tiger reserve established in India...
+              </Text>
+              <Link
+                href="https://tourism.bihar.gov.in/en/destinations/west-champaran/valmiki-tiger-reserve"
+                _hover={{ textDecorationLine: "none" }}
+              >
+                <Button
+                  bgColor={"teal"}
+                  color={"white"}
+                  _hover={{ color: "teal", bgColor: "white", border: "2px" }}
+                >
+                  Read More
+                </Button>
+              </Link>
+            </Box>
+            <Box border={"1px"}>
+              <Image
+                src="https://th.bing.com/th/id/R.21d94e41e3f1097a086574fc7915c0f8?rik=uSSvQup61tHAMw&riu=http%3a%2f%2f3.bp.blogspot.com%2f_Y-CUxvBqU_o%2fTHY88CQG0BI%2fAAAAAAAAAAg%2ff3FVkV0LB14%2fs1600%2fimage002.jpg&ehk=lFnhcLb8O2Rp%2fLmHgybf9xcordlF7KXGHbLTi8VaGQw%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1"
+                w={"100%"}
+                h={"100%"}
+              />
+            </Box>
+          </Flex>
+        </Box>
+        <chakra.h3
+          fontFamily={"Work Sans"}
+          fontWeight={"bold"}
+          fontSize={30}
+          textAlign={"left"}
+          color={"black"}
+        >
+          Social Feed
+        </chakra.h3>
+        <Box mt="10px">
+          <Image src="https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/social-media-feed/social-feed-image.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg" />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Bihar;
