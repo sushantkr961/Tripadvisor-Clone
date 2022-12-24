0dbe8b4a84ff8efed67a391d0e6a49371511b0c1
Nipendra Soni <rsoni2843@gmail.com>
2022-12-24 15:03:18 +0530

    api changed from localhost to vercel api

--- Backend/build/index.js
+++ Backend/build/index.js
@@ -4,9 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+require("dotenv/config");
+const connectDb_1 = __importDefault(require("./src/config/connectDb"));
+const user_routes_1 = __importDefault(require("./src/routes/UserRoutes/user.routes"));
+const hotel_routes_1 = __importDefault(require("./src/routes/HotelRoutes/hotel.routes"));
 const app = (0, express_1.default)();
-const port = 5000;
-app.get("/", (_, res) => {
-    res.status(200).send("WORKING");
+const mongoUrl = process.env.MONGO_URL || "";
+const port = process.env.PORT || 8080;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.all("/", (req, res) => res.send("Wroking Home Route"));
+app.use("/user", user_routes_1.default);
+app.use("/hotel", hotel_routes_1.default);
+(0, connectDb_1.default)(mongoUrl);
+app.listen(port, () => {
+    console.log("Connected to PORT", port);
 });
-app.listen(port, () => console.log(`Running on port ${port}`));
